135b66925d8c5527c4d80b3b1542b6b9909bab97
Andrii Mishtal <mishtal.andriy@gmail.com>
2020-08-12 02:49:37 +0300

    Updates

---
+++ precache-manifest.d4fe61860d6d07ac5bbc85de3b4fb8e0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "251016a629026d290ea9f85545077c30",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e91fd1356908f3b2e4f6",
+    "url": "./static/css/main.8d183450.chunk.css"
+  },
+  {
+    "revision": "2e00d72e443c3b35203a",
+    "url": "./static/js/2.669e5c4a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.669e5c4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e91fd1356908f3b2e4f6",
+    "url": "./static/js/main.ec3cb035.chunk.js"
+  },
+  {
+    "revision": "4e0eef9267ed46c1c31c",
+    "url": "./static/js/runtime-main.4d21a353.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "./static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "./static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "./static/media/spinner.235b9b3e.gif"
+  }
+]);
